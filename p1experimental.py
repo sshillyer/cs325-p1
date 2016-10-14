ee8b8c7ae463b6d8c4c23fdcb5d14934fd200141
shawn.hillyer@gmail.com <shawn.hillyer@gmail.com>
2016-10-14 12:48:22 -0700

    Add p1experiental.py file

---
+++ p1experimental.py
@@ -0,0 +1,9 @@
+#CS 325_400_F2016 Project1 - Correctness Tests
+# Jesse Thoren, Shawn Hillyer, Jason Goldfine-Middleton
+
+from analysis_helpers import *
+from A1_Enumeration import *
+from A2_BetterEnumeration import *
+from A3_DivideAndConquer import *
+from A4_LinearTime import *
+import sys
