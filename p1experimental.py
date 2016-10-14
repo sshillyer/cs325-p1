988b8a9c15290ebc2972fbff0346c2afede1b682
shawn.hillyer@gmail.com <shawn.hillyer@gmail.com>
2016-10-14 13:02:38 -0700

    Start n-value generator

--- p1experimental.py
+++ p1experimental.py
@@ -7,3 +7,17 @@ from A2_BetterEnumeration import *
 from A3_DivideAndConquer import *
 from A4_LinearTime import *
 import sys
+
+def set_n_values(start, step_size, qty):
+    start = start
+    step = step_size
+    qty = qty
+    stop = start + (qty * step)
+    return start, stop, step
+
+start, stop, step = set_n_values(10, 5, 20)
+alg_1_n_values = range(start, stop, step)
+
+print(alg_1_n_values)
+for v in alg_1_n_values:
+    print(v)
