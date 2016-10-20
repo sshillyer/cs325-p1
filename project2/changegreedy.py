0a75d6e40362a3779ca630f504a2a34d8523e418
jesseakt <thorenje@oregonstate.edu>
2016-10-20 08:27:38 -0700

    Made greedy take out all coins of a denomination at once rather than one at a time. Speeds up considerably for large amounts

--- project2/changegreedy.py
+++ project2/changegreedy.py
@@ -11,12 +11,12 @@ def changegreedy(D, a):
 
     #Loop through denomination array backwards to test big coins first.
     for i in range(len(D)-1, -1, -1):
-        while(a>=D[i]):
-            minarr[i] = minarr[i]+1
-            a = a - D[i]
-            minamt = minamt +1
+        if(a>=D[i]):
+            minarr[i] = a//D[i]
+            minamt = minamt + a//D[i]
+            a = a%D[i]
     return minarr, minamt
-#
+
 # #User input
 # user_array = input("Enter array of denominations in increasing order: ")
 # for char in user_array:
@@ -25,14 +25,14 @@ def changegreedy(D, a):
 # user_array = user_array.split(',')
 # for i in range(0,len(user_array)):
 #     user_array[i] = int(user_array[i])
-#
+#     
 # user_amount = int(input("Enter an amount to make change for: "))
-#
+# 
 # #Function call
 # start_time = time.time()
 # arr, mincoins = changegreedy(user_array, user_amount)
 # end_time = time.time() - start_time
-#
+# 
 # #Results
 # print(arr)
 # print("You need to use", mincoins, "coins.")
